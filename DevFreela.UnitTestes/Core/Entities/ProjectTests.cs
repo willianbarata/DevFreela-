15fc5bf81281e4e738d26391af76ce6aaeba3952
Willian Barata <willianbarata@gmail.com>
2023-08-25 21:57:56 -0300

    Criando o primeiro test unitário

---
+++ DevFreela.UnitTestes/Core/Entities/ProjectTests.cs
@@ -0,0 +1,36 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevFreela.UnitTestes.Core.Entities
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void TestIfProjectStartWorks()
+        {
+            var project = new Project("Nome de teste", "Descrição de teste", 1, 2, 10000);
+
+            Assert.Equal(ProjectStatusEnum.Created,project.Status);
+            Assert.Null(project.StartedAt);
+            
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            project.Start();
+
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+
+
+        }
+    }
+}
